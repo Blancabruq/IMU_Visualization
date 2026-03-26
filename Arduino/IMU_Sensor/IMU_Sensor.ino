59c52715b7cf7b533e0a625e284fdfc197d56603
Blancabruq <blanca.bruquerol@usp.ceu.es>
2026-03-26 17:47:19 +0100

    Second sensor and csv string

--- Arduino/IMU_Sensor/IMU_Sensor.ino
+++ Arduino/IMU_Sensor/IMU_Sensor.ino
@@ -3,22 +3,43 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+// Initialization of both sensors 
 Adafruit_BNO055 sensor1 = Adafruit_BNO055(1, 0x28);
+Adafruit_BNO055 sensor2 = Adafruit_BNO055(2, 0x29);
 
 void setup() {
   Serial.begin(115200);
   delay(2000); 
-  if (sensor1.begin()) sensor1.setExtCrystalUse(true);
+  
+  
+  if (!sensor1.begin()) {
+    Serial.println("Error S1");
+  } else {
+    sensor1.setExtCrystalUse(true);
+  }
+
+  if (!sensor2.begin()) {
+    Serial.println("Error S2");
+  } else {
+    sensor2.setExtCrystalUse(true);
+  }
 }
 
 void loop() {
   imu::Quaternion q1 = sensor1.getQuat();
+  imu::Quaternion q2 = sensor2.getQuat();
   
-  // Sending data in CSV format for Unity: W, X, Y, Z
+  // Sending Sensor 1 data 
   Serial.print(q1.w(), 4); Serial.print(",");
   Serial.print(q1.x(), 4); Serial.print(",");
   Serial.print(q1.y(), 4); Serial.print(",");
-  Serial.println(q1.z(), 4); 
+  Serial.print(q1.z(), 4); Serial.print(","); 
+
+  // Sending Sensor 2 data 
+  Serial.print(q2.w(), 4); Serial.print(",");
+  Serial.print(q2.x(), 4); Serial.print(",");
+  Serial.print(q2.y(), 4); Serial.print(",");
+  Serial.println(q2.z(), 4); 
 
   delay(50); 
 }
